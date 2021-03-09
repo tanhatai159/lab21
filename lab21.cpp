e64d3cdd9dfca997518304fed9e625dd69f8e52a
tanhatai159 <gun159@outlook.com>
2021-03-09 17:34:32 +0700

    completed

--- lab21.cpp
+++ lab21.cpp
@@ -28,7 +28,63 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(real*c.imag)+(imag*c.real));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber(((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2)),((imag*c.real)-(real*c.imag))/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber((s*c.real)+(0*c.imag),(s*c.imag)+(0*c.real));
+}
+
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	return ComplexNumber(((s*c.real)+(0*c.imag))/(pow(c.real,2)+pow(c.imag,2)),((0*c.real)-(s*c.imag))/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return 1;
+	else return 0;
+}
+
+bool operator==(double s,const ComplexNumber &c){
+	if(s == c.real && 0 == c.imag) return 1;
+	else return 0;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return (atan2(imag,real)*180)/M_PI;
+}
+
+ostream & operator<<(ostream &os,const ComplexNumber &c){
+	if(c.real == 0){
+		if(c.imag > 0) return os << ""<< c.imag <<"i";
+		else if(c.imag == 0) return os << "0";
+		else return os << ""<< c.imag <<"i";
+	}
+    else{
+		if(c.imag > 0) return os << ""<< c.real <<"+"<< c.imag <<"i";
+		else if(c.imag == 0) return os << ""<< c.real <<"";
+		else return os << ""<< c.real <<""<< c.imag <<"i";
+	}
+	
+}
+
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
